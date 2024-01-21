cf27f814697434d337b3c651581fe4d898d9d7d7
Selene <149156667+joanapq@users.noreply.github.com>
2024-01-21 20:28:45 +0000

    added comments and improved presentability of javascript code

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # Daily-Planner-App---edX-Bootcamp-Challenge-7
  A simple calendar application that allows a user to save events for each hour of the day.
+# Code Quiz
+
+This repository contains my answer to the Module 6 Challenge of the Web Development edX bootcamp: https://jpquental90.github.io/Code-Quiz---edX-Bootcamp-Challenge-6/
+
+## Description
+
+My task was to create a time quiz application that uses local storage to store highscores. Some starter code was provided by edX in HTML and CSS files. A gif demonstrating the desired local of the final product was provided as a guide. 
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Code](#code)
+* [Credits](#credits)
+* [License](#license)
+
+## Installation
+
+N/A
+
+## Usage
+
+#### Initial page with introduction to quiz challenge
+
+<center>
+
+![Screenshot of initial page with quiz introduction and 'Start' button](assets/images/Screenshot%20app%201.png)
+</center>
+
+---
+
+#### Example of quiz question and options, as well as the message that display when the selected answer is incorrect:
+
+<center>
+
+![Screenshot of one of the questions when the selected answer is incorrect](assets/images/Screenshot%20app%202.png)
+</center>
+
+You can also see above that functioning of the timer, with the number reduced from what it was at the start.
+
+---
+
+#### Final screen after completion of quiz, allowing user to submit their initials and save their score:
+
+<center>
+
+![Screenshot of final screen with text box for user initials and a 'Submit' button](assets/images/Screenshot%20app%203.png)
+</center>
+
+---
+
+After clicking 'Submit', the Highscores page is displayed. This page is also accessible at all times from the top left corner of the quiz pages. 
+
+#### Highscores display, with submissions listed by order of score (first priority) and time left (second priority):
+
+<center>
+
+![High scores page with 3 submissions which are ordered by score and time left](assets/images/Screenshot%20app%204.png)
+</center>
+
+## Code
+
+I have made some changes to the HTML and especially the CSS file, but the main work was completed on JavaScript. I created all 3 JavaScript files suggested as links (in the index.html and highscores.html files) in the starter code. Comments were added throughout to explain each of the steps taken to create the app functionality and interaction with the user.
+
+#### Questions file
+
+<center>
+
+![Screenshot of JavaScript code with list of quiz questions](assets/images/Screenshot%20code%201.png)
+</center>
+
+---
+
+#### Logic file
+
+<center>
+
+![Screenshot of JavaScript code with logic behind quiz](assets/images/Screenshot%20code%202.png)
+</center>
+
+---
+
+#### Highscores file
+
+<center>
+
+![Screenshot of JavaScript code with logic behind the display of highscores from local storage](assets/images/Screenshot%20code%203.png)
+</center>
+
+---
+
+## Credits
+
+I have studied DOM manipulation on Digital Ocean and Google was used to research the solution for each coding step. I have used the edX bootcamp Xpert Learning Assistant to help troubleshoot some of the problems with the code. I also consulted some of the material from the edX bootcamp classes.
+
+## License
+
+Licensed under the MIT license.
--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,38 @@
+//To make sure the code runs successfully:
 $(document).ready(function() {
 
-    // Display the current day at the top of the calendar when a user opens the planner.
-    $('#currentDay').text(dayjs().format('dddd, MMMM Do'));
+//Displays the current day at the top of the calendar when a user opens the planner:
+$('#currentDay').text(dayjs().format('dddd, MMMM Do'));
 
-// Present time blocks for standard business hours when the user scrolls down.
+//Making main container centered on the page:
 const container = $('.container').addClass('row').css({
     'margin-left': 'auto',
     'margin-right': 'auto',
 });
 
-// Create a spacer for better spacing.
+//Creating a spacer between the black line and the time schedule:
 $('<div>').css({'height': '30px'}).appendTo(container);
 
+//Array with all the hours of the working day:
 const businessHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
+//For/of loop to successfully add timeblock lines in a bootstrap grid system to make up the full time schedule covering business hours:
 for (const hour of businessHours) {
+    // Adding the relevant HTML elements with the appropriate classes:
     const timeBlockContainer = $('<div class="row"></div>').appendTo(container);
     const hourDiv = $('<div class="col-lg-1 col-sm-2 hour"></div>').text(hour).appendTo(timeBlockContainer);
     const timeBlockDiv = $('<div class="col-lg-10 col-sm-8 time-block"></div>').appendTo(timeBlockContainer);
     const saveBtn = $('<button class="col-lg-1 col-sm-2 saveBtn"><i class="fa fa-lock"></i></button>').appendTo(timeBlockContainer);
     const textAreaEl = $('<textarea class="eventInput"></textarea>').appendTo(timeBlockDiv);
 
-    // Set initial value from local storage.
+    //Retrieving information from local storage:
     const savedEvent = localStorage.getItem(`event-${hour}`);
+    
     if (savedEvent) {
-    textAreaEl.val(savedEvent);
+        textAreaEl.val(savedEvent);
     }
 
+    //Adjusting the style of the left and middle columns:
     hourDiv.css({
         textAlign: 'right',
         paddingTop: '15px',
@@ -34,17 +40,18 @@ for (const hour of businessHours) {
     })
 
     textAreaEl.css({
-    width: '100%',
-    height: '100%',
+        width: '100%',
+        height: '100%',
     });
 
+    //Replicating mock-up styles - what happens when text area is in focus and when icon is hovered over:
     textAreaEl.focus(function() {
         $(this).css({
         outline: 'none',
         border: '3px solid #06aed5'
         });
-      });
-  
+    });
+
     textAreaEl.blur(function() {
         $(this).css('border', '');
     });
@@ -54,28 +61,29 @@ for (const hour of businessHours) {
     function () { $(this).find('i').css('color', 'white'); }
     );
 
-    // Save event to local storage on button click.
+    //Adding 'click' event to save the added input to local storage, displaying an alert when it has been saved:
     saveBtn.on('click', function() {
-    let eventText = textAreaEl.val();
-    let currentHour = hourDiv.text().trim();
-    localStorage.setItem(`event-${currentHour}`, eventText);
-    alert('Event saved successfully!');
+        let eventText = textAreaEl.val();
+        let currentHour = hourDiv.text().trim();
+        localStorage.setItem(`event-${currentHour}`, eventText);
+        alert('Event saved successfully!');
     });
 
-    // Color-code each time block based on past, present, and future.
+    //Colour-coding each timeblock based on past, present, and future (including conversion of time range to only go up to 12 and to consider whether event is AM or PM):
     let blockHour = parseInt(hour.replace(/[^\d]/g, ''), 10);
     const isPM = hour.includes("PM");
 
     if (isPM && blockHour !== 12) {
-    blockHour += 12;
+        blockHour += 12;
     }
 
+    //Colour grey is applied to past hours, colour red is applied to current hour and colour green is applied to future hours:
     if (blockHour < dayjs().hour()) {
-    timeBlockDiv.addClass('past');
+        timeBlockDiv.addClass('past');
     } else if (blockHour === dayjs().hour()) {
-    timeBlockDiv.addClass('present');
+        timeBlockDiv.addClass('present');
     } else {
-    timeBlockDiv.addClass('future');
+        timeBlockDiv.addClass('future');
     }
 }
 });
